239b79e7b64f0c8d809f0d84a0d2c27518a24484
Reuben <gathiireuben@gmail.com>
2023-08-28 12:55:17 +0300

    Added Recipe data class

--- app/src/main/java/com/example/a30daysapp/model/Recipe.kt
+++ app/src/main/java/com/example/a30daysapp/model/Recipe.kt
@@ -1,6 +1,12 @@
 package com.example.a30daysapp.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 data class Recipe(
-    val dayOfMonth: Int,
-    
+    @StringRes val dayOfMonth: Int,
+    @StringRes val recipeSummary: Int,
+    @DrawableRes val imageRes: Int,
+    @StringRes val recipeDescriptionTitle: Int,
+    @StringRes val recipeDescriptionBody: Int,
 )
