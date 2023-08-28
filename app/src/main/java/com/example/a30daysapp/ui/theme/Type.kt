2179e276b8f865a33a2815628b3ace2141442df1
Reuben <gathiireuben@gmail.com>
2023-08-28 12:49:25 +0300

    Added text styles and poppins font family

--- app/src/main/java/com/example/a30daysapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/a30daysapp/ui/theme/Type.kt
@@ -2,18 +2,41 @@ package com.example.a30daysapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.a30daysapp.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_bold),
+    Font(R.font.poppins_medium)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = Poppins,
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
